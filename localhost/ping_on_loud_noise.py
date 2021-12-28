9b640ebabc805afdb6691fa4276bfca19b46765d
JustATin555 <usingenglish.2014@gmail.com>
2021-12-28 14:16:45 +0800

    Running average for ping_on_loud_noise

--- localhost/ping_on_loud_noise.py
+++ localhost/ping_on_loud_noise.py
@@ -7,23 +7,25 @@ from rekognition import analyze_photo
 
 from send_info import send_info
 
-#Initialisation for PyAudio
+# PyAudio Settings
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 120
+RECORD_SECONDS = 1
 
-#Object
+# Init PyAudio
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
- 
+
+# Store previous data for running average
+history = []
+
 while True:
-    total=0
+    total = 0
 
     #Now we read data from device for around one second
-    for i in range(0,2):
-        #l,data = inp.read()
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
 
         #oreo_sound.append(data)
@@ -33,15 +35,26 @@ while True:
         
         time.sleep(.0001)
 
-    #any scaling factor
+    # Converts sound data into "decibel" scale
     total = math.log(total)
 
-    if total > 11:
+    # Clear old history
+    if len(history) >= 60:
+        history.pop(0)
+
+    # Update history
+    history.append(total)
+
+    # Running average
+    avg = sum(history) / len(history)
+    print(avg)
+
+    if avg > 11:
         pic_path = take_picture()
 
         info = analyze_photo(pic_path)
 
-        info['decibel'] = total
+        info['decibel'] = avg
 
         send_info(info)
 
